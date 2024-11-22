39ba886a5ac7f7b854a131399a72364133743767
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2024-11-21 20:42:21 -0700

    More Changes

---
+++ src/implementations/MyArrayList.java
@@ -0,0 +1,282 @@
+package implementations;
+import java.util.NoSuchElementException;
+import utilities.Iterator;
+import utilities.ListADT;
+
+
+/**
+ * A dynamic array-based implementation of the ListADT interface.
+ * This class provides basic list operations such as adding, removing, 
+ * retrieving, and modifying elements, along with some utility methods 
+ * like ensuring capacity and converting the list to an array.
+ *
+ * @param <E> the type of elements held in this list
+ */
+public class MyArrayList<E> implements ListADT<E> {
+    private E[] array;
+    private int size;
+    private static final int INITIAL_CAPACITY = 10;
+
+    
+    /**
+     * Constructs an empty list with an initial capacity of 10.
+     */
+    
+    @SuppressWarnings("unchecked")
+    public MyArrayList() {
+        array = (E[]) new Object[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+      /**
+     * Returns the number of elements in this list.
+     *
+     * @return the size of the list
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+    
+     /**
+     * Clears the list by resetting its size and array.
+     */
+    @Override
+    public void clear() {
+        array = (E[]) new Object[INITIAL_CAPACITY];
+        size = 0;
+    }
+    
+     /**
+     * Adds an element at the specified index, shifting subsequent elements 
+     * to the right. Ensures the list has enough capacity to accommodate the 
+     * new element.
+     *
+     * @param index the position to add the element at
+     * @param toAdd the element to be added
+     * @return true if the element is successfully added
+     * @throws NullPointerException if the element is null
+     * @throws IndexOutOfBoundsException if the index is out of bounds
+     */
+
+    @Override
+    public boolean add(int index, E toAdd) throws NullPointerException, IndexOutOfBoundsException {
+        if (toAdd == null) throw new NullPointerException("Element cannot be null");
+        if (index < 0 || index > size) throw new IndexOutOfBoundsException("Index out of bounds");
+
+        ensureCapacity();
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = toAdd;
+        size++;
+        return true;
+    }
+
+    /**
+     * Adds an element to the end of the list. Ensures the list has enough 
+     * capacity to accommodate the new element.
+     *
+     * @param toAdd the element to be added
+     * @return true if the element is successfully added
+     * @throws NullPointerException if the element is null
+     */
+    @Override
+    public boolean add(E toAdd) throws NullPointerException {
+        if (toAdd == null) throw new NullPointerException("Element cannot be null");
+
+        ensureCapacity();
+        array[size++] = toAdd;
+        return true;
+    }
+
+     /**
+     * Adds all elements from the specified list to the end of this list.
+     *
+     * @param toAdd the list of elements to be added
+     * @return true if all elements are successfully added
+     * @throws NullPointerException if the specified list is null
+     */
+    @Override
+    public boolean addAll(ListADT<? extends E> toAdd) throws NullPointerException {
+        if (toAdd == null) throw new NullPointerException("Collection cannot be null");
+
+        Iterator<? extends E> iterator = toAdd.iterator();
+        while (iterator.hasNext()) {
+            add(iterator.next());
+        }
+        return true;
+    }
+    
+    /**
+     * Retrieves the element at the specified index.
+     *
+     * @param index the index of the element to retrieve
+     * @return the element at the specified index
+     * @throws IndexOutOfBoundsException if the index is out of bounds
+     */
+    @Override
+    public E get(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException("Index out of bounds");
+        return array[index];
+    }
+
+     /**
+     * Removes the element at the specified index and shifts the remaining 
+     * elements to the left.
+     *
+     * @param index the index of the element to remove
+     * @return the removed element
+     * @throws IndexOutOfBoundsException if the index is out of bounds
+     */
+    @Override
+    public E remove(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException("Index out of bounds");
+
+        E removedElement = array[index];
+        System.arraycopy(array, index + 1, array, index, size - index - 1);
+        array[--size] = null;
+        return removedElement;
+    }
+     /**
+     * Removes the first occurrence of the specified element from the list.
+     *
+     * @param toRemove the element to be removed
+     * @return the removed element or null if not found
+     * @throws NullPointerException if the element to remove is null
+     */
+
+    @Override
+    public E remove(E toRemove) throws NullPointerException {
+        if (toRemove == null) throw new NullPointerException("Element cannot be null");
+
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(toRemove)) {
+                return remove(i);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Replaces the element at the specified index with the specified element.
+     *
+     * @param index the index of the element to replace
+     * @param toChange the new element to replace the old element
+     * @return the old element that was replaced
+     * @throws NullPointerException if the element to add is null
+     * @throws IndexOutOfBoundsException if the index is out of bounds
+     */
+    @Override
+    public E set(int index, E toChange) throws NullPointerException, IndexOutOfBoundsException {
+        if (toChange == null) throw new NullPointerException("Element cannot be null");
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException("Index out of bounds");
+
+        E oldElement = array[index];
+        array[index] = toChange;
+        return oldElement;
+    }
+
+    /**
+     * Checks if the list is empty (i.e., size is 0).
+     *
+     * @return true if the list is empty, false otherwise
+     */
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+    /**
+     * Checks if the specified element is present in the list.
+     *
+     * @param toFind the element to find
+     * @return true if the element is present, false otherwise
+     * @throws NullPointerException if the element to find is null
+     */
+    @Override
+    public boolean contains(E toFind) throws NullPointerException {
+        if (toFind == null) throw new NullPointerException("Element found in current list and should not be found.");
+
+    for (int i = 0; i < size; i++) {
+        if (array[i] != null && array[i].equals(toFind)) {
+            return true;
+        }
+    }
+    return false;
+    }
+
+    /**
+     * Converts the list to an array. If the provided array is large enough, 
+     * it is used; otherwise, a new array is created.
+     *
+     * @param toHold the array to hold the elements of the list
+     * @return the array containing the elements of the list
+     * @throws NullPointerException if the array to hold the elements is null
+     */
+    @Override
+    public E[] toArray(E[] toHold) throws NullPointerException {
+        if (toHold == null) throw new NullPointerException("Array cannot be null");
+
+        if (toHold.length < size) {
+            return (E[]) java.util.Arrays.copyOf(array, size, toHold.getClass());
+        }
+        System.arraycopy(array, 0, toHold, 0, size);
+        if (toHold.length > size) {
+            toHold[size] = null;
+        }
+        return toHold;
+    }
+
+      /**
+     * Converts the list to an array. A new array is created if necessary.
+     *
+     * @return an array containing all of the elements in the list
+     */
+    @Override
+    public Object[] toArray() {
+        return java.util.Arrays.copyOf(array, size);
+    }
+
+    /**
+     * Returns an iterator over the elements in the list.
+     *
+     * @return an iterator for the list
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return new MyArrayListIterator();
+    }
+
+     /**
+     * Ensures the list has enough capacity to accommodate additional elements.
+     * If necessary, the capacity is doubled.
+     */
+    private void ensureCapacity() {
+        if (size == array.length) {
+            array = java.util.Arrays.copyOf(array, array.length * 2);
+        }
+    }
+
+    /**
+     * An iterator for the MyArrayList class.
+     */
+    private class MyArrayListIterator implements Iterator<E> {
+        private int currentIndex = 0;
+
+        /**
+         * Checks if there are more elements to iterate over.
+         *
+         * @return true if there are more elements, false otherwise
+         */
+        @Override
+        public boolean hasNext() {
+            return currentIndex < size;
+        }
+
+        
+        @Override
+        public E next() throws NoSuchElementException {
+            if (!hasNext()) throw new NoSuchElementException("No more elements");
+            return array[currentIndex++];
+        }
+    }
+}
---
+++ src/implementations/MyDLLNode.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package implementations;
+
+/**
+ *
+ * @author Marian
+ */
+public class MyDLLNode<E> {
+    E data;
+    MyDLLNode<E> next;
+    MyDLLNode<E> prev;
+
+    public MyDLLNode(E data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
